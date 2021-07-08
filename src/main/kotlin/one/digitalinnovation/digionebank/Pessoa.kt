6347f30cac78e3119133a46f47811d131593f974
boss <lucaseduardocoelho@gmail.com>
2021-07-08 13:12:52 -0300

    adding members and function

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,7 +4,7 @@ class Pessoa {
     var nome:String = "Lucas"
     var  cpf:String = "123.123.123-11" // var private cpf:String = "123.123.123-11" -- only visible for this class
     private set // para nao ser possivel alterar o CPF
-
+    fun dadosDaPessoa() = "$cpf e $nome"
     inner class endereco {
         var rua:String = "rua azul 35"
     }
@@ -12,10 +12,8 @@ class Pessoa {
 
 fun main() {
     val lucas = Pessoa()
+    println(lucas.dadosDaPessoa())
 
 
-    println(Pessoa().nome)
-    println(lucas.nome)
-    println(lucas.endereco().rua)
 
 }
