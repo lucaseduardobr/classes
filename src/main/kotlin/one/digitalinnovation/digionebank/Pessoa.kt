e434b46bca948b8737a87f54963e2f67533292ac
boss <lucaseduardocoelho@gmail.com>
2021-07-08 03:16:23 -0300

    modifying cpf property from set and get to only get

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,7 +2,8 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome:String = "Lucas"
-    var cpf:String = "123.123.123-11"
+    var  cpf:String = "123.123.123-11" // var private cpf:String = "123.123.123-11" -- only visible for this class
+    private set // para nao ser possivel alterar o CPF
 
     inner class endereco {
         var rua:String = "rua azul 35"
@@ -12,8 +13,9 @@ class Pessoa {
 fun main() {
     val lucas = Pessoa()
 
-    println(Pessoa().cpf)
-    println(lucas.cpf)
+
+    println(Pessoa().nome)
+    println(lucas.nome)
     println(lucas.endereco().rua)
 
 }
