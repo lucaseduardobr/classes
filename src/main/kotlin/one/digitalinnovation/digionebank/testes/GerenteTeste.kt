e41e2c419ce37718a602418681872274024c2737
boss <lucaseduardocoelho@gmail.com>
2021-07-13 14:37:44 -0300

    Gerente.kt is a child class of Funcionario.kt which means it get data through instance from Funcionario.kt

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/GerenteTeste.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank.testes
+
+
+import one.digitalinnovation.digionebank.Gerente
+
+fun main() {
+    val maria = Gerente(nome="maria almeida" , cpf="123456700", salario = 5000.00)
+    imprimeRelatorioFuncionario.imprime(maria)
+
+
+}
+
+
