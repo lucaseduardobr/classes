a33ba025767a32c590d5cc3edd40062926a020b1
boss <lucaseduardocoelho@gmail.com>
2021-07-14 11:47:14 -0300

    Created to verify if a person is auth or not

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/AutenticacaoTeste.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Logavel
+
+class AutenticacaoTeste {
+    fun autentica(logavel: Logavel)= println(logavel.login()) // aqui ele pega logavel e depois usa a funcao login dessa funcao logavel
+
+}
